f2e30cf65b3b5f9b1ad8998d35c19e55d9241caa
Aniruddha Shastri <aniruddha.shastri@gmail.com>
2014-11-16 04:02:14 -0500

    changes

--- src/augur/org/AugurMapreduceTool.java
+++ src/augur/org/AugurMapreduceTool.java
@@ -4,7 +4,6 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.hadoop.conf.Configured;
@@ -19,10 +18,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class AugurMapreduceTool extends Configured implements Tool { 
+public class AugurMapreduceTool extends Configured implements Tool {
 
 	public static class Map extends Mapper<Text, Text, Text, IndividualMetric> {
 		public void map(Text movieName, Text comment, Context context)
@@ -89,78 +89,95 @@ public class AugurMapreduceTool extends Configured implements Tool {
 	public static class Reduce extends
 			Reducer<Text, IndividualMetric, Text, Text> {
 		public void reduce(Text movieName,
-				Iterable<IndividualMetric> metricList, Context context) throws IOException, InterruptedException {
+				Iterable<IndividualMetric> metricList, Context context)
+				throws IOException, InterruptedException {
 			MovieMetric movieMetric = new MovieMetric();
 			int aCount = 0;
 			int cCount = 0;
 			int tCount = 0;
 			int yCount = 0;
-			final Iterator<IndividualMetric> metricIter = metricList.iterator();
-			while(metricIter.hasNext()) {
-				IndividualMetric metric = metricIter.next();
+			for (IndividualMetric metric : metricList) {
 				MetricType type = MetricType.None;
-				if(metric.type == CommentType.Numeric) {
-					if(metric.source == CommentSource.RottenTomatoes) {
-						if(metric.exType == ExtraCommentType.Audience) {
+				if (metric.type == CommentType.Numeric) {
+					if (metric.source == CommentSource.RottenTomatoes) {
+						if (metric.exType == ExtraCommentType.Audience) {
 							type = MetricType.AudienceScore;
 						}
-						if(metric.exType == ExtraCommentType.Critic) {
+						if (metric.exType == ExtraCommentType.Critic) {
 							type = MetricType.CriticsScore;
 						}
 					}
-					if(metric.source == CommentSource.YouTube) {
-						if(metric.exType == ExtraCommentType.Likes) {
+					if (metric.source == CommentSource.YouTube) {
+						if (metric.exType == ExtraCommentType.Likes) {
 							type = MetricType.VideoLikes;
 						}
-						if(metric.exType == ExtraCommentType.Dislikes) {
+						if (metric.exType == ExtraCommentType.Dislikes) {
 							type = MetricType.VideoDislikes;
 						}
-						if(metric.exType == ExtraCommentType.Views) {
+						if (metric.exType == ExtraCommentType.Views) {
 							type = MetricType.VideoViews;
 						}
 					}
-					movieMetric.metrics.put(type, metric.metricValue);
+					movieMetric.metrics.put(type, new DoubleWritable(metric.metricValue.get()));
 				}
-				if(metric.type == CommentType.Text) {
+				if (metric.type == CommentType.Text) {
 					double tempMetricValue = 0;
-					if(metric.source == CommentSource.RottenTomatoes) {
-						if(metric.exType == ExtraCommentType.Audience) {
-							tempMetricValue = (movieMetric.metrics.containsKey(MetricType.AudienceComment)) ? movieMetric.metrics.get(MetricType.AudienceComment).get() : 0.0;
+					if (metric.source == CommentSource.RottenTomatoes) {
+						if (metric.exType == ExtraCommentType.Audience) {
+							tempMetricValue = (movieMetric.metrics
+									.containsKey(MetricType.AudienceComment)) ? movieMetric.metrics
+									.get(MetricType.AudienceComment).get()
+									: 0.0;
 							tempMetricValue += metric.metricValue.get();
 							type = MetricType.AudienceComment;
 							aCount++;
 						}
-						if(metric.exType == ExtraCommentType.Critic) {
-							tempMetricValue = (movieMetric.metrics.containsKey(MetricType.CriticsComment)) ? movieMetric.metrics.get(MetricType.CriticsComment).get() : 0.0;
+						if (metric.exType == ExtraCommentType.Critic) {
+							tempMetricValue = (movieMetric.metrics
+									.containsKey(MetricType.CriticsComment)) ? movieMetric.metrics
+									.get(MetricType.CriticsComment).get() : 0.0;
 							tempMetricValue += metric.metricValue.get();
 							type = MetricType.CriticsComment;
 							cCount++;
 						}
 					}
-					if(metric.source == CommentSource.Twitter) {
-						tempMetricValue = (movieMetric.metrics.containsKey(MetricType.TwitterComment)) ? movieMetric.metrics.get(MetricType.TwitterComment).get() : 0.0;
+					if (metric.source == CommentSource.Twitter) {
+						tempMetricValue = (movieMetric.metrics
+								.containsKey(MetricType.TwitterComment)) ? movieMetric.metrics
+								.get(MetricType.TwitterComment).get() : 0.0;
 						tempMetricValue += metric.metricValue.get();
 						type = MetricType.TwitterComment;
 						tCount++;
 					}
-					if(metric.source == CommentSource.YouTube) {
-						tempMetricValue = (movieMetric.metrics.containsKey(MetricType.VideoComment)) ? movieMetric.metrics.get(MetricType.VideoComment).get() : 0.0;
+					if (metric.source == CommentSource.YouTube) {
+						tempMetricValue = (movieMetric.metrics
+								.containsKey(MetricType.VideoComment)) ? movieMetric.metrics
+								.get(MetricType.VideoComment).get() : 0.0;
 						tempMetricValue += metric.metricValue.get();
 						type = MetricType.VideoComment;
 						yCount++;
 					}
-					movieMetric.metrics.put(type, new DoubleWritable(tempMetricValue));
+					movieMetric.metrics.put(type, new DoubleWritable(
+							tempMetricValue));
 				}
 			}
-			double Avg = movieMetric.metrics.get(MetricType.AudienceComment).get() / aCount;
-			movieMetric.metrics.put(MetricType.AudienceComment, new DoubleWritable(Avg));
-			Avg = movieMetric.metrics.get(MetricType.CriticsComment).get() / cCount;
-			movieMetric.metrics.put(MetricType.CriticsComment, new DoubleWritable(Avg));
-			Avg = movieMetric.metrics.get(MetricType.TwitterComment).get() / tCount;
-			movieMetric.metrics.put(MetricType.TwitterComment, new DoubleWritable(Avg));
-			Avg = movieMetric.metrics.get(MetricType.VideoComment).get() / yCount;
-			movieMetric.metrics.put(MetricType.VideoComment, new DoubleWritable(Avg));
-			
+			double Avg = movieMetric.metrics.get(MetricType.AudienceComment)
+					.get() / aCount;
+			movieMetric.metrics.put(MetricType.AudienceComment,
+					new DoubleWritable(Avg));
+			Avg = movieMetric.metrics.get(MetricType.CriticsComment).get()
+					/ cCount;
+			movieMetric.metrics.put(MetricType.CriticsComment,
+					new DoubleWritable(Avg));
+			Avg = movieMetric.metrics.get(MetricType.TwitterComment).get()
+					/ tCount;
+			movieMetric.metrics.put(MetricType.TwitterComment,
+					new DoubleWritable(Avg));
+			Avg = movieMetric.metrics.get(MetricType.VideoComment).get()
+					/ yCount;
+			movieMetric.metrics.put(MetricType.VideoComment,
+					new DoubleWritable(Avg));
+
 			context.write(movieName, new Text(movieMetric.toString()));
 		}
 	}
@@ -171,12 +188,21 @@ public class AugurMapreduceTool extends Configured implements Tool {
 	}
 
 	@Override
-	public int run(String[] args) throws Exception {
+	public int run(String[] argsFull) throws Exception {
 		Job job = Job.getInstance(getConf());
-		
-		job.addArchiveToClassPath(new Path("s3://augurframework/include/nlpjars.tar.gz"));
-		job.addFileToClassPath(new Path("s3://augurframework/include/nlp_file.properties"));
-		
+
+		job.addArchiveToClassPath(new Path(
+				"s3://augurframework/include/ejml-0.23.jar"));
+		job.addArchiveToClassPath(new Path(
+				"s3://augurframework/include/stanford-corenlp-3.4.1.jar"));
+		job.addArchiveToClassPath(new Path(
+				"s3://augurframework/include/stanford-corenlp-3.4.1-models.jar"));
+		job.addFileToClassPath(new Path(
+				"s3://augurframework/include/nlp_file.properties"));
+//
+//		GenericOptionsParser.getLibJars(getConf());
+		String[] args = new GenericOptionsParser(argsFull).getRemainingArgs();
+
 		job.setJarByClass(AugurMapreduceTool.class);
 
 		job.setMapOutputKeyClass(Text.class);
@@ -189,10 +215,10 @@ public class AugurMapreduceTool extends Configured implements Tool {
 
 		job.setInputFormatClass(KeyValueTextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
-		
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
 		job.waitForCompletion(true);
 		return 0;
 	}
@@ -216,11 +242,16 @@ enum ExtraCommentType {
 
 class MovieMetric implements Writable {
 	Map<MetricType, DoubleWritable> metrics = new HashMap<MetricType, DoubleWritable>();
+	String[] MetricTypeString = { "None", "TwitterComment", "AudienceComment",
+			"CriticsComment", "AudienceScore", "CriticsScore", "VideoComment",
+			"VideoLikes", "VideoDislikes", "VideoViews", "BoxOfficeCollection" };
 
 	public String toString() {
 		String str = new String();
-		for(MetricType key : MetricType.values()) {
-			str = str + metrics.get(key).toString() + " ";
+		for (MetricType key : MetricType.values()) {
+			if (metrics.containsKey(key)) {
+				str = str + MetricTypeString[key.ordinal()] + "=" + metrics.get(key).toString() + " ";
+			}
 		}
 		return str;
 	}
@@ -241,6 +272,13 @@ class MovieMetric implements Writable {
 }
 
 class IndividualMetric implements Writable {
+	IndividualMetric() {
+		source = CommentSource.None;
+		type = CommentType.None;
+		exType = ExtraCommentType.None;
+		metricValue = new DoubleWritable();
+	}
+
 	IndividualMetric(CommentSource cSrc, CommentType cType,
 			ExtraCommentType cExType, double num) {
 		source = cSrc;
